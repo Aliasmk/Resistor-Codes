303e06e625b9c18c5f8aa62c72479e460525cf65
Michael K <xrcomptec@gmail.com>
2014-06-17 21:14:54 -0400

    Decimal Fix
    Now will work for resistors with 0.1 or 0.01 multipliers

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Compling Resistor.java...
-javac Resistor.java
+javac resistor.java
 cmd /k "echo Compile Attempt Complete
 @echo on
--- resistor.java
+++ resistor.java
@@ -1,84 +1,132 @@
 public class resistor
 {
+	//global var to be passed to all methods
+	double resistance;
+	String rawInput;
+	//0-9 are normal colors, 10 is Gold (0.1 multiplier) and 11 is Silver (0.01 multiplier)
+	String[] digitColors = {"Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Gray","White","Gold","Silver"};
 	
-	int resistance;
-	//0-9 are normal colors, 10 is Silver (0.01 multiplier) and 11 is Gold (0.1 multiplier)
-	String[] digitColors = {"Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Gray","White","Silver","Gold"};
-	
-	public resistor(int resistval)
+	public resistor(double resistval, String input)
 	{
+		//pass arg to global var
+		rawInput = input;
 		resistance = resistval;
-		start();
+		
+	}
+	
+	public static void usage()
+	{
+		System.out.println("Usage: java resistor [value | color] [resistance_value | color1 color2 color3 color4]");
+		System.exit(0);
 	}
 	
 	public static void main(String[] args)
 	{
-		int resistval = 0;
+		
 		if(args.length > 0)
 		{	
-			try
+			if(args[0].equals("value"))
 			{
-				
-				resistval = Integer.parseInt(args[0]);
+				double resistval = 0;
+				//Catch if the User Enters something that isnt an Integer
+				try
+				{
+					resistval = Double.parseDouble(args[1]);		
+				}
+				catch(Exception e)
+				{
+					usage();
+				}
+				//Start the program as an instance
+				resistor r = new resistor(resistval, args[1]);
+				r.valueToColor();
 			}
-			catch(Exception e)
+			else if(args[0].equals("value"))
 			{
-				System.out.println("Invalid Input. Please enter an integer. (Decimals are not supported yet.)");
-				System.exit(1);
+				//nothing yet
 			}
-			
-			resistor r = new resistor(resistval);
+			else
+			{
+				usage();
+			}
 		}
 		else
 		{
-			System.out.println("Usage: java resistor resistance_value");
-			System.exit(0);
+			//User forgot vars. Show usage.
+			usage();
 		}
 	}
 	
-	public void start()
+	public void valueToColor()
 	{
-		
-		String inputstring = Integer.toString(resistance);
-		int length = inputstring.length();
-		
+		//Set Vars
+		String inputstring = Double.toString(resistance);
+		int length = rawInput.length();		
 		boolean decimal = false;
 		int zeroafterpoint = 0;
 		
-		//print value
-		System.out.println("Resistor Value: " +resistance +" ohms");
-		//tranfer into char array
-		
+		//Tranfer into char array
 		char[] input = new char[length];
+		
+		
+	
+		
 		for(int i = 0; i<length; i++)
-		{
-			if(decimal)
-				zeroafterpoint++;
-			input[i] = inputstring.charAt(i);
-			if(input[i] == '.' && input[0] == 0)
+		{	
+			if(rawInput.charAt(i) == '.')
+			{
 				decimal = true;
+			}
 		}
 		
+		if(decimal)
+		{
+			boolean record = false;
+			int shiftedIndex = 0;
+			for(int i = 0; i<length; i++)
+			{
+				if(record)
+				{
+					input[shiftedIndex] = inputstring.charAt(i);
+					zeroafterpoint++;
+					shiftedIndex++;
+				}
+				
+				if(rawInput.charAt(i) == '.')
+				{
+					record = true;
+				}
+				
+				
+			}
+		}
+		else
+		{
+			for(int i = 0; i<length; i++)
+			{
+				input[i] = inputstring.charAt(i);
+			}
+		}
+		
+		
+		
+		//Set Each Value to Correspond to Color Array at the Top
 		int digit1 = Character.getNumericValue(input[0]);
 		int digit2 = Character.getNumericValue(input[1]);
-		int multiplier = 0;
-		
+		int multiplier;
 		if(decimal)
 			multiplier = 9+zeroafterpoint;
 		else
 			multiplier = length-2;
 		
+		//OUTPUT
+		System.out.println("Resistor Value: " +resistance +" ohms");
 		System.out.println("Digit 1: " +digit1);
 		System.out.println("Digit 2: " +digit2);
-		
 		if(decimal)
-			System.out.println("Multiplier: " +Math.pow(10,-(zeroafterpoint-2)));
+			System.out.println("Multiplier: " +Math.pow(10,-(zeroafterpoint)));
 		else
 			System.out.println("Multiplier: " +Math.pow(10,length-2));
-		
-		
 		System.out.println("Colors: " +digitColors[digit1] +" " +digitColors[digit2] +" " +digitColors[multiplier]);
-		
-		
 	}
 }
