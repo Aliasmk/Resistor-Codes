2cdf1a3184ab9fa9fa32727c613f7c9abfcfc6ce
Michael K <xrcomptec@gmail.com>
2014-06-17 22:54:28 -0400

    Two Way
    The program can now decode colors into resistor values. Tolerance added
    to ColorToValue but not yet to ValueToColor.

--- resistor.java
+++ resistor.java
@@ -1,17 +1,29 @@
 public class resistor
-{
-	//global var to be passed to all methods
+{	
+	//global vars to be passed to valueToColor
 	double resistance;
 	String rawInput;
+	
+	//global vars to be passed to colorToValue
+	String[] resistcolors;
+	
 	//0-9 are normal colors, 10 is Gold (0.1 multiplier) and 11 is Silver (0.01 multiplier)
 	String[] digitColors = {"Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Gray","White","Gold","Silver"};
+	double[] digitTolerance = {0,1,2,0,0,0.5,0.25,0.1,0,0,5,10};
+
 	
 	public resistor(double resistval, String input)
 	{
 		//pass arg to global var
 		rawInput = input;
 		resistance = resistval;
+		valueToColor();
 		
+	}
+	
+	public resistor(String[] colors)
+	{
+		colorToValue(colors);
 	}
 	
 	public static void usage()
@@ -39,11 +51,18 @@ public class resistor
 				}
 				//Start the program as an instance
 				resistor r = new resistor(resistval, args[1]);
-				r.valueToColor();
+				
 			}
-			else if(args[0].equals("value"))
+			else if(args[0].equals("color") || args[0].equals("colors") || args[0].equals("colour") || args[0].equals("colours"))
 			{
-				//nothing yet
+				//from here we start with arg[1]
+				String[] inputcolors = new String[args.length-1];
+				
+				for(int i = 0; i < inputcolors.length; i++)
+				{
+					inputcolors[i] = args[i+1].toUpperCase();
+				}	
+				resistor r = new resistor(inputcolors);
 			}
 			else
 			{
@@ -54,6 +73,52 @@ public class resistor
 		{
 			//User forgot vars. Show usage.
 			usage();
+		}
+	}
+	
+	public void colorToValue(String[] color)
+	{
+		if(color.length == 4)
+		{
+			double[] digits = new double[4];
+		
+			for(int i = 0; i < color.length; i++)
+			{
+				for(int c = 0; c < digitColors.length; c++)
+				{
+					if(digitColors[c].equalsIgnoreCase(color[i]))
+					{
+						digits[i] = c;
+					}
+				}
+			}
+			
+			int digit1 = (int) digits[0];
+			int digit2 = (int) digits[1];
+			
+			String firstTwo = Integer.toString(digit1)+Integer.toString(digit2);
+			int firstTwoInt = Integer.parseInt(firstTwo);
+			double value;
+			if(digits[2] <9)
+			{
+				value = firstTwoInt * Math.pow(10,digits[2]);
+				System.out.print((int)value);
+			}
+			else
+			{
+				value = firstTwoInt * Math.pow(10,9-digits[2]);
+				System.out.print(value);
+			}
+			double tolerance = digitTolerance[(int)digits[3]];
+			if(tolerance != 0)
+				System.out.println(" ohms (+/- " +tolerance +"%)");
+			else
+				System.out.println("Invalid input: No such tolerance color");
+		}
+		else
+		{
+			System.out.println("Only 4-color resistors are supported at this time.");
+			System.exit(1);
 		}
 	}
 	
